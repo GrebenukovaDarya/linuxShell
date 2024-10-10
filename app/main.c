f72be2be1c0ffe8b0fcb87621efc9a734ca9c4a1
s0188223 <loved4248@gmail.com>
2024-10-10 16:52:03 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,12 +1,19 @@
+#include <string.h>
 #include <stdio.h>
-
 int main() {
-   //Uncomment this block to pass the first stage
-   printf("$ ");
-   fflush(stdout);
 
-  // Wait for user input
+  printf("$ ");
+
+  fflush(stdout);
+
   char input[100];
+
   fgets(input, 100, stdin);
+ 
+  input[strlen(input) - 1] = '\0';
+
+  printf("%s: command not found\n", input);
+
   return 0;
+
 }
